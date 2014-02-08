9e0361537211390e74a2afe946b5ff86994130dc
Ben <JMartin5532@gmail.com>
2014-02-08 16:12:34 -0500

    added punch spaghetti to shooter

--- Shooter.cpp
+++ Shooter.cpp
@@ -8,7 +8,9 @@ const float Shooter::SPEED_AXISPOWER = 0.5f;
 Shooter::Shooter(uint8_t axisMod,
                  uint8_t attractMod, uint32_t attractChan,
                  uint8_t clampMod, uint32_t clampFChan, uint32_t clampRChan,
-                 uint8_t bobModA, uint32_t bobChanA, uint8_t bobModB, uint32_t bobChanB)
+                 uint8_t bobModA, uint32_t bobChanA, uint8_t bobModB, uint32_t bobChanB,
+                 uint8_t wormMod, uint32_t wormChan,
+                 unit8_t punchMod,unit32_t punchFChan,unit32_t punchRChan)
 {
     axis = new CANJaguar(axisMod);
     attractor = new Talon(attractMod, attractChan);
@@ -18,6 +20,8 @@ Shooter::Shooter(uint8_t axisMod,
     //shooterJoy -> addJoyFunctions(&buttonHelper,(void*)this,CLAMP_DOWN);
     bobTheEncoder = new Encoder(bobModA, bobChanA, bobModB, bobChanB);
     bobTheEncoder->Start();
+    wormDrive = new Talon(wormMod,wormChan);
+    puncher = new DoubleSolenoid(punchMod,punchFChan,punchRChan);
     isPickingUp = false;
     robot -> update -> addFunctions(&updateHelper, (void*)this);
 }
@@ -27,6 +31,9 @@ Shooter::~Shooter()
     delete axis;
     delete attractor;
     delete clamper;
+    delete bobTheEncoder;
+    delete wormGear;
+    delete puncher;
 }
 
 void Shooter::pitchUp()
@@ -97,6 +104,26 @@ void Shooter::clampUp()
     clamp = up;
 }
 
+void Shooter::wormPull()
+{
+    wormGear -> Set(-SPEED_WORM);
+    wormIsPulling = true;
+}
+
+void Shooter::wormStop()
+{
+    wormGear -> Set(0);	
+    wormIsPulling = false;
+}
+
+void Shooter::punch()
+{
+	if(isLoaded){
+		pneumatics -> setVectorValues(PUNCH_TIME, puncher, DoubleSolenoid::kForward);
+		isLoaded = false;
+	}
+}
+
 //X to fire
 void Shooter::buttonHelper(void* objPtr, uint32_t button){
     Shooter* shooterObj=(Shooter*)objPtr;
@@ -146,14 +173,22 @@ void Shooter::update()
     else
     {
         if(isPickingUp){
-        	pitchAngle(SHOOTING_POSITION);
-        	isPickingUp = false;
+            pitchAngle(SHOOTING_POSITION);
+            isPickingUp = false;
         }
-    	if (!isPitchingUp){
-    	    clampUp();
-    	}
+        if (!isPitchingUp){
+            clampUp();
+        }
+    }
+    if (wormIsPulling){
+        if (Sensors::getInfraredLoad())
+        {
+           	wormStop();
+           	isLoaded = true;
+        }
     }
 }
+
 
 void Shooter::updateHelper(void* instName)
 {
--- Shooter.h
+++ Shooter.h
@@ -8,6 +8,7 @@
 #include "controls.h"
 #include "Pneumatics.h"
 #include "SmoothJoystick.h"
+#include "Sensors.h"
 
 class Shooter
 {
@@ -15,7 +16,9 @@ public:
     Shooter(uint8_t axisMod,
                  uint8_t attractMod, uint32_t attractChan,
                  uint8_t clampMod, uint32_t clampFChan, uint32_t clampRChan,
-                 uint8_t bobModA, uint32_t bobChanA, uint8_t bobModB, uint32_t bobChanB);
+                 uint8_t bobModA, uint32_t bobChanA, uint8_t bobModB, uint32_t bobChanB,
+                 uint8_t wormMod, uint32_t wormChan,
+                 uint8_t punchMod,uint32_t punchFChan,uint32_t punchRChan);
     ~Shooter();
     enum Clamp {down, up};
     Clamp clamp;
@@ -28,27 +31,36 @@ public:
     void autoClamp();
     void clampDown();//Clamps down on ball & pulls
     void clampUp();//opens clamp
+    void wormPull();
+    void wormStop();
+    void punch();
+    
     CANJaguar* axis;
     Talon* attractor;
     DoubleSolenoid* clamper;
     Pneumatics* pneumatics;
     SmoothJoystick* shooterJoy;
     Encoder* bobTheEncoder;
-    //Blah* puncher;
+    Talon* wormGear;
+    DoubleSolenoid* puncher;
+   
     const static float SPEED_AXISPOWER;
     const static float SPEED_ATTRACTOR = 0.5f;
     const static double TIME = 0.1;
+    const static double PUNCH_TIME = 0.1;
+    
     bool isPickingUp;
     bool isPitchingUp;
     bool isPitchingDown;
+    bool wormIsPulling;
+    bool isLoaded;
     double currentPos;
     double destinationPos;
     double originPos;
     static const double SHOOTING_POSITION = 45;
     static const double PICKUP_POSITION = 135;
-
+    static const float SPEED_WORM = 1.0f;
     static void buttonHelper(void* objPtr, uint32_t button);
-
     void update();
     static void updateHelper(void* instName);
 };
