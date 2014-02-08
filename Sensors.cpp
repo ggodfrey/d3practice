b05cb992c3e5d1ac3abc29250220886849564e38
ChampD <amothwo@gmail.com>
2014-02-08 10:33:15 -0500

    Added Sensors object, fixed capitalization of Sensor class

--- Sensors.cpp
+++ Sensors.cpp
@@ -1,10 +1,10 @@
 #include <AnalogChannel.h>
 #include "ports.h"
-#include "Sensors.h" //<--- this :D
+#include "Sensors.h"
 #include "main.h"
 #include "612.h"
 
-sensors::sensors(uint8_t USMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, uint32_t ISCHANNEL,
+Sensors::Sensors(uint8_t USMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, uint32_t ISCHANNEL,
                  uint8_t ILMODNUMBER, uint32_t ILCHANNEL)
 {
     ultrasonic = new AnalogChannel(USMODNUMBER, USCHANNEL);
@@ -13,19 +13,19 @@ sensors::sensors(uint8_t USMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, u
      robot -> update -> addFunctions(&updateHelper, (void*)this);
 }
 
-sensors::~sensors()
+Sensors::~Sensors()
 {
 
 }
 
 //all values are in centemeters
 
-float sensors::getInfraredShooter()
+float Sensors::getInfraredShooter()
 {
     return (infraredShooter->GetVoltage() * 18.777);
 }
 
-bool sensors::getInfraredLoad()
+bool Sensors::getInfraredLoad()
 {
     float loadDistance = infraredLoad->GetVoltage() * 18.777;
     if(loadDistance < 2)
@@ -38,14 +38,14 @@ bool sensors::getInfraredLoad()
     }
 }
 
-float sensors::getUltrasonic()
+float Sensors::getUltrasonic()
 {
     return (((ultrasonic->GetVoltage()) / 9.8) * 2.54);
 }
 
 void updateHelper(void* instName)
 {
-    sensors* sensorObj = (sensors*)instName;
+    Sensors* sensorObj = (Sensors*)instName;
     sensorObj->getInfraredShooter();
     sensorObj->getInfraredLoad();
     sensorObj->getUltrasonic();
--- Sensors.h
+++ Sensors.h
@@ -1,20 +1,24 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
 #include <AnalogChannel.h>
 #include "ports.h"
-#include "main.h"
-#include "612.h"
 
-class sensors
+class Sensors
 {
+public:
     AnalogChannel* ultrasonic;
     AnalogChannel* infraredShooter;
     AnalogChannel* infraredLoad;
 
-    sensors(uint8_t ISMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, uint32_t ISCHANNEL,
+    Sensors(uint8_t ISMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, uint32_t ISCHANNEL,
             uint8_t ILMODNUMBER, uint32_t ILCHANNEL);
-    ~sensors();
-public:
+    ~Sensors();
+
     float getInfraredShooter();
     bool getInfraredLoad();
     float getUltrasonic();
     static void updateHelper(void* instName);
 };
+
+#endif
