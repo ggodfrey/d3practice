c59e5bd5254a160732aaa76e60f4e4502577fb92
anidev <anidev.aelico@gmail.com>
2014-02-11 22:09:39 -0500

    Minor fixes to Sensors, also does not need update helper

--- Sensors.cpp
+++ Sensors.cpp
@@ -4,13 +4,14 @@
 #include "main.h"
 #include "612.h"
 
+// everything in inches
+
 Sensors::Sensors(uint8_t USMODNUMBER, uint32_t USCHANNEL, uint8_t ISMODNUMBER, uint32_t ISCHANNEL,
                  uint8_t ILMODNUMBER, uint32_t ILCHANNEL)
 {
     ultrasonic = new AnalogChannel(USMODNUMBER, USCHANNEL);
     infraredShooter = new AnalogChannel(ISMODNUMBER, ISCHANNEL);
     infraredLoad = new AnalogChannel(ILMODNUMBER, ILCHANNEL);
-     robot -> update -> addFunctions(&updateHelper, (void*)this);
 }
 
 Sensors::~Sensors()
@@ -22,13 +23,14 @@ Sensors::~Sensors()
 
 float Sensors::getInfraredShooter()
 {
-    return (infraredShooter->GetVoltage() * 18.777);
+    // 18.77cm/V = 7.38976in/V
+    return (infraredShooter->GetVoltage() * 7.38976);
 }
 
 bool Sensors::getInfraredLoad()
 {
-    float loadDistance = infraredLoad->GetVoltage() * 18.777;
-    if(loadDistance < 2)
+    float loadDistance = infraredLoad->GetVoltage() * 7.38976;
+    if(loadDistance < LOAD_THRESHOLD)
     {
         return true;
     }
@@ -40,14 +42,6 @@ bool Sensors::getInfraredLoad()
 
 float Sensors::getUltrasonic()
 {
-    return (((ultrasonic->GetVoltage()) / 9.8) * 2.54);
+    // 9.8mV/in = 0.0098V/in
+    return ((ultrasonic->GetVoltage()) / 0.0098);
 }
-
-void updateHelper(void* instName)
-{
-    Sensors* sensorObj = (Sensors*)instName;
-    sensorObj->getInfraredShooter();
-    sensorObj->getInfraredLoad();
-    sensorObj->getUltrasonic();
-}
-
